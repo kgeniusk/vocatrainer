6ca0bb32575e0a0faadd6ffe8249a162eb66a2a4
Mose Kim (Macbook Air) <kgeniusk@gmail.com>
2023-08-07 12:26:48 +0200

    added some more elements

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def video():
 @app.route('/html')
 def html():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=True)
